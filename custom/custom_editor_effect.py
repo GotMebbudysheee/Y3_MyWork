5962b2981d118f0371f0952aa1a09e689b1944ba
GotMebbuddysheee <test@test.com>
2023-11-17 20:37:40 +0800

    auto save : 20:37:40

--- custom/custom_editor_effect.py
+++ custom/custom_editor_effect.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134223142: {'model_id': 134223142, 'name': u'portal', 'describe': 134223142, 'key': 134223142, 'res_identify': u'', 'scale': 1.0, 'uid': 134223142, 'source_effect': 100707}, 134242904: {'model_id': 134242904, 'name': u'blade', 'describe': 134242904, 'key': 134242904, 'res_identify': u'', 'scale': 1.0, 'uid': 134242904, 'source_effect': 102325}, 134260750: {'model_id': 134260750, 'name': u'arena', 'describe': 134260750, 'key': 134260750, 'res_identify': u'', 'scale': 1.0, 'uid': 134260750, 'source_effect': 103631}}
--- custom/custom_model.py
+++ custom/custom_model.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134223142: {'name': u'portal', 'model_id': 134223142, 'meshes': [], 'effect_path': 'Fx_Effect/Cartoon/Fx_Sence/Decoration/Decoration_dungeon_entrance_02_copy', 'custom': True, 'effect_keep': True, 'effect_max_life': -1}, 134242904: {'name': u'blade', 'model_id': 134242904, 'meshes': [], 'effect_path': 'Fx_Effect/Realism/Fx_Skill/Knife/Knife_AX_male_attack01_03_copy', 'custom': True, 'effect_keep': False, 'effect_max_life': 0.500000007}, 134260750: {'name': u'arena', 'model_id': 134260750, 'meshes': [], 'effect_path': 'Fx_Effect/OtherType/Prop/Prop_zuozhe_qipai0003_copy', 'custom': True, 'effect_keep': True, 'effect_max_life': -1}}
